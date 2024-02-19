07a663ee3bae2859b88c2891940678e0ae09bed5
andrz <andrz@ENDRIU>
2024-02-20 00:41:13 +0100

    Odp do zadania 2

---
+++ LibApp/Controllers/API/BooksController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LibApp.Models;
+using LibApp.ViewModels;
+using System.Linq;
+using LibApp.Data;
+
+namespace LibApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var books = _context.Books.ToList();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBook(int id)
+        {
+            var book = _context.Books.SingleOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook([FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bookInDb = _context.Books.SingleOrDefault(b => b.Id == id);
+            if (bookInDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.SaveChanges();
+
+            return Ok(bookInDb);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var bookInDb = _context.Books.SingleOrDefault(b => b.Id == id);
+            if (bookInDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(bookInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
+
+
+
+
