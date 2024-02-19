c56d0d09459259586c027f2b4f07bd93c2c762cd
andrz <andrz@ENDRIU>
2024-02-19 22:21:51 +0100

    Odp do zadania 1

--- LibApp/Models/Book.cs
+++ LibApp/Models/Book.cs
@@ -5,16 +5,19 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(100)]
+        [Required(ErrorMessage = "Tytuł jest wymagany!")]
+        [StringLength(99, ErrorMessage = "Tytuł nie może być dłuższy niż 99 znaków!")]
         public string Title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Autor jest wymagany!")]
         public string Author { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Gatunek jest wymagany!")]
         public Genre Genre { get; set; }
         public int GenreId { get; set; }
         public DateTime DateAdded { get; set; }
+        [Required(ErrorMessage = "Data wydania jest wymagana!")]
         public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Liczba sztuk jest wymagana")]
+        [Range(1, 20, ErrorMessage = "Liczbe sztuk powinna mieścić się w przedziale 1-20")]
         public byte NumberInStock { get; set; }
     }
 }
